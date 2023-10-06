6a234a494710a45a8e5e00a9ac7b9a6e9429e097
Felipe Lemos <felipe.lemos2556@gmail.com>
2023-10-05 23:31:31 -0300

    Add files via upload

---
+++ prjExemplo4/src/Exemplo4.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Dimas
+ */
+public class Exemplo4 {
+    public static void main(String[] args) {
+        double numero, resultado;
+        
+        numero = Math.random() * 100;
+        
+        for (int x=1; x <= 10; x++){
+            resultado = numero * x;
+            System.out.println(numero + " x " + x + " = " + resultado);
+        }
+    }    
+}
